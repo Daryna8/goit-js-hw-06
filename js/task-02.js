aee3547f288b1253902845f505704f82f0372ccf
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-10-30 18:03:43 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+function ingredientTemplate (ingredient) {
+const ingredientElement = document.createElement('li');
+ingredientElement.textContent = `${ingredient}`;
+ingredientElement.classList.add('js-ingredients-item');
+  return ingredientElement;
+};
+
+function ingredientsTemplate(ingredients) {
+  return ingredients.map(ingredientTemplate);
+}
+
+
+function renderIngredients(ingredients) {
+  const ingredientsList = document.querySelector('#ingredients');
+  const ingredientList = ingredientsTemplate(ingredients);
+  ingredientsList.append(...ingredientList);
+};
+
+renderIngredients(ingredients);
