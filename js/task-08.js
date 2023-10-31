cae090674530bb4a41f7906ae99aa64f17f00dbe
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-10-31 20:26:35 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+    
+    if (email === '' || password === '') {
+        alert(`Всі поля повинні бути заповнені!`);
+    }
+
+     const result = { email, password };
+    
+    console.log(result);
+    event.target.reset();
+};
