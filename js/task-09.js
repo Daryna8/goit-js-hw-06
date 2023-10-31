41b4aeb73044fda0d9ef5408025f6171a0a567d6
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-10-31 20:42:17 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
